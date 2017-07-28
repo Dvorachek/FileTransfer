9bc7b80157a35f876a625482bb6b76179f65805c
Dvorachek <Dvorachek@users.noreply.github.com>
2017-07-27 19:07:23 -0700

    Add files via upload
    added progress bar

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 
-os.chdir("..")
-BUFF_SIZE = 1024
+# os.chdir("..") # for exe
+BUFF_SIZE = 4096
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = "192.168.0.47"
 port = 8888
@@ -29,28 +29,40 @@ while True:
 
 #receive
 if msg == 'r':
+    print 'Receiving File...\n'
     print s.recv(BUFF_SIZE)
     s.send(raw_input())
     file_name = s.recv(BUFF_SIZE) # add while True loop for retries
+    
+    
     if file_name == 'invalid':
         print "Invalid choice or incorrect spelling"
         quit()
+    file_size = s.recv(BUFF_SIZE)
     f = open(file_name, 'wb')
     l = s.recv(BUFF_SIZE)
-    print 'Receiving File...'
-    file_size = sys.getsizeof(l) - 33
+    print 'the file is: {}\nthe size is: {}'.format(file_name, file_size)
+    transfered = sys.getsizeof(l) - 33
+    t = -1
     while(l):
-        print "=>[{}] bytes transfered: {}".format(file_name, file_size)
+        progress = '#'
+        remaining = '-'
+        percentage = float("{:0.2f}".format(transfered/float(file_size)*100))
+        if t != percentage:
+            print '\r[{}{}] %{}'.format(progress*int(percentage), remaining*(100-int(percentage)), percentage),
+            t = percentage
+        else:
+            pass
+        # print "=>[{}] bytes transfered: {}".format(file_name, file_size)
         f.write(l)
         l = s.recv(BUFF_SIZE)
-        file_size += sys.getsizeof(l) - 33
+        transfered += sys.getsizeof(l) - 33
     f.close()
 
 # send
 elif msg == 's':
     print s.recv(BUFF_SIZE)
-    files = [file for file in os.listdir(os.getcwd())]
-    files.remove('client')
+    files = [file for file in os.listdir(os.getcwd()) if os.path.isfile(file)]
     print '\n'.join(files)
     while True:
         file_name = raw_input()
@@ -60,18 +72,30 @@ elif msg == 's':
             print 'Try again..'
         else:
             s.send(file_name)
+            file_size = str(int(os.path.getsize(file_name)))
+            s.send(file_size)
             break
     f = open(file_name, 'rb')
-    print 'Sending file...'
+    print 'the file is: {}\nthe size is: {}'.format(file_name, file_size)
+    print 'Sending file...\n'
     l = f.read(BUFF_SIZE)
-    file_size = sys.getsizeof(l) -33
+    t = -1
+    transfered = sys.getsizeof(l) - 33
     while(l):
-        print "=>[{}] bytes transfered: {}".format(file_name, file_size)
+        progress = '#'
+        remaining = '-'
+        percentage = float("{:0.2f}".format(transfered/float(file_size)*100))
+        if t != percentage:
+            print '\r[{}{}] %{}'.format(progress*int(percentage), remaining*(100-int(percentage)), percentage),
+            t = percentage
+        else:
+            pass
+        # print "=>[{}] bytes transfered: {}".format(file_name, file_size)
         s.send(l)
         l = f.read(BUFF_SIZE)
-        file_size += sys.getsizeof(l) - 33
+        transfered += sys.getsizeof(l) - 33
     f.close()
-
+print 'BYE BYE'
 s.shutdown(socket.SHUT_WR)
 s.close()
 
